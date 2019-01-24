fdd236f6adda150884897849c580e6696e3d5ac9
skykingit <skykingit@aliyun.com>
2019-01-24 11:05:33 +0800

    Create pchain.rb
    add homebrew rules

---
+++ pchain.rb
@@ -0,0 +1,22 @@
+class Pchain < Formula
+  homepage 'https://github.com/pchain-org/pchain'
+  url 'https://github.com/pchain-org/pchain.git'
+  version '0.0.1'
+  
+  # Require El Capitan at least
+  depends_on :macos => :el_capitan
+
+  depends_on :xcode => :build
+  depends_on 'go' => :build
+  
+  def install
+    ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
+    system "go", "env" 
+    system "make", "pchain"
+    bin.install 'bin/pchain'
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/pchain", "version"
+  end
+end
